6351c4bf564be100b9dcda17308049f8db30746a
shsen-tori <nazu26255@gmail.com>
2026-02-27 14:19:19 +0900

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE = "v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
